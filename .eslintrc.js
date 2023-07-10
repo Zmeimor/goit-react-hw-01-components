af9e96585e3cfca78fdcb4deaace296a54030e2a
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-07-10 21:56:34 +0200

    о

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,50 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:react/recommended",
+//         "plugin:react/jsx-runtime"
+//     ],
+//     "overrides": [
+//         {
+//             "env": {
+//                 "node": true
+//             },
+//             "files": [
+//                 ".eslintrc.{js,cjs}"
+//             ],
+//             "parserOptions": {
+//                 "sourceType": "script"
+//             }
+//         }
+//     ],
+//     "parserOptions": {
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "react"
+//     ],
+//     "rules": {
+//     }
+// }
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
+    "extends": ["react-app"],
     "rules": {
-    }
-}
+    },
+    "overrides": [
+      {
+        "files": ["**/*.js?(x)"],
+        "rules": {
+  // ******** add ignore rules here *********
+          "react/no-unescaped-entities": "off",
+        //   "react/display-name": "off",
+        //   "react/prop-types": "off",
+        }
+      }
+    ]
+  }
